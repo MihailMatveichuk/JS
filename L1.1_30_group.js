204807796dd0c5a94e9b6b9983f41080e4d2facc
MihailMatveichuk <mihkailmatveichuk@gmail.com>
2022-06-16 21:26:17 +0300

    Add the HW1 of JS again

--- L1.1_30_group.js
+++ L1.1_30_group.js
@@ -1,65 +1,65 @@
-let age_1 = 17
+let age_1 = 10
 let age_2 = 18
 let age_3 = 60
     /*1 *:
         Преобразовать написанный код в 26 - 33 пунктах в функцию, принимающую на вход возраст.*/
-/*const checkAge = function(age) {
+const checkAge = function(age) {
     if (age < age_1) {
         console.log("age_1 == ", age_1)
     } else if (age < age_2) {
         console.log("You don’ t have access cause your age is " + age_1 + " It’ s less then")
-    }else if (age >= age_2 && age < age_3) {
-            console.log("Welcome!")
-    }else if (age > age_3) {
-            console.log("Keep calm and look Culture channel")
+    } else if (age >= age_2 && age <= age_3) {
+        console.log("Welcome!")
     } else {
-        console.log("Technical work")
+        console.log("Keep calm and look Culture channel")
     }
 }
 checkAge(17)
+checkAge(18)
+checkAge(61)
     //Вывести в консоль результат работы функции с возрастами 17, 18, 61
     /*2*:
     Преобразовать задание 1* таким образом, чтобы первым делом в функции проверялся тип данных. И если он не Number - кидалась ошибка.*/
-    
-    const checkAge = function(age) {
-        if (typeof(age) === 'number') {
-            if (age < age_1) {
-                console.log("age_1 == ", age_1)
-            } else if (age < age_2) {
-                console.log("You don’ t have access cause your age is " + age_1 + " It’ s less then")
-            } else if (age >= age_2 && age < age_3) {
-                console.log("Welcome!")
-            } else if (age > age_3) {
-                console.log("Keep calm and look Culture channel")} 
+
+/*const checkAge = function(age) {
+    if (typeof(age) === 'number') {
+        if (age < age_1) {
+            console.log("age_1 == ", age_1)
+        } else if (age < age_2) {
+            console.log("You don’ t have access cause your age is " + age_1 + " It’ s less then")
+        } else if (age >= age_2 && age <= age_3) {
+            console.log("Welcome!")
         } else {
-            console.log("Technical work")
-            }
+            console.log("Keep calm and look Culture channel")
+        }
+    } else {
+        console.log("Wrong varuable")
     }
-    checkAge(17)
-    checkAge(18)
-    checkAge(61)
-    checkAge('61')
-        /*3**:
-        Преобразовать 2* таким образом, чтобы значение '2' (строка в которой лежит ТОЛЬКО ЦИФРА) пропускалось, преобразовываясь в number*/
-    
-   /* const checkAge = function(age) {
-        if (typeof age === 'number' || age == Number(age)) {
-            if (age < age_1) {
-                console.log("age_1 == ", age_1)
-            } else if (age < age_2) {
-                console.log("You don’ t have access cause your age is " + age_1 + " It’ s less then")
-            }else if (age >= age_2 && age < age_3) {
-                console.log("Welcome!")
-            }else if (age > age_3) {
-                console.log("Keep calm and look Culture channel")
-            } else {
-                console.log("Technical work")} 
+}
+checkAge(17)
+checkAge(18)
+checkAge(61)
+checkAge('61')
+    /*3**:
+    Преобразовать 2* таким образом, чтобы значение '2' (строка в которой лежит ТОЛЬКО ЦИФРА) пропускалось, преобразовываясь в number*/
+
+/*const checkAge = function(age) {
+    if (age == Number(age)) {
+        if (age < age_1) {
+            console.log("age_1 == ", age_1)
+        } else if (age < age_2) {
+            console.log("You don’ t have access cause your age is " + age_1 + " It’ s less then")
+        } else if (age >= age_2 && age <= age_3) {
+            console.log("Welcome!")
         } else {
-            console.log("Wrong varuable")
-            }
+            console.log("Keep calm and look Culture channel")
+        }
+    } else {
+        console.log("Wrong varuable")
     }
-    checkAge(17)
-    checkAge(18)
-    checkAge(61)
-    checkAge('61')
-    checkAge('ar')*/
+}
+checkAge(1)
+checkAge(18)
+checkAge(60)
+checkAge('61')
+checkAge('ar')*/
---
+++ L1_30_group_script.js
@@ -0,0 +1,20 @@
+ let age_1 = 17
+ let age_2 = 18
+ let age_3 = 60
+ const age = prompt("Insert value", " ")
+ const checkAge = function(age) {
+     if (age == Number(age)) {
+         if (age < age_1) {
+             alert("age_1 == ", age_1)
+         } else if (age < age_2) {
+             alert("You don’ t have access cause your age is " + age_1 + " It’ s less then")
+         } else if (age >= age_2 && age <= age_3) {
+             alert("Welcome!")
+         } else {
+             alert("Keep calm and look Culture channel")
+         }
+     } else {
+         alert("Wrong varuable")
+     }
+ }
+ checkAge(age);
